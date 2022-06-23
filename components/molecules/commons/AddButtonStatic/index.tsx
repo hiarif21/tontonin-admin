ee8bb9d8992f888d595f60db71c088d6caff3da8
Arif Hidayat <84622058+hiarif21@users.noreply.github.com>
2022-06-23 12:19:40 +0700

    update add button static

--- components/molecules/commons/AddButtonStatic/index.tsx
+++ components/molecules/commons/AddButtonStatic/index.tsx
@@ -9,7 +9,7 @@ const AddButtonStatic = ({ title, onClick }: AddButtonStaticProps) => {
   return (
     <button
       onClick={onClick}
-      className="fixed bottom-5 right-5 flex items-center gap-3 rounded-xl bg-blue-500 px-5 py-3 text-sm font-bold text-white shadow-md shadow-blue-500/50">
+      className="fixed bottom-5 right-5 flex items-center gap-3 rounded-xl bg-blue-500 px-5 py-3 text-sm font-bold capitalize text-white shadow-md shadow-blue-500/50">
       <Icons icon={'Plus'} color={'white'} />
       {title}
     </button>
