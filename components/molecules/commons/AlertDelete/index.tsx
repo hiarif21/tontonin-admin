53e31fc29ce738417875e30d75c8d1f4594c3cf4
Arif Hidayat <84622058+hiarif21@users.noreply.github.com>
2022-06-23 10:58:21 +0700

    add alert delete

---
+++ components/molecules/commons/AlertDelete/index.tsx
@@ -0,0 +1,39 @@
+import { useRef } from 'react';
+import Modal from '../../../atoms/Modal';
+
+interface AlertDeleteProps {
+  data: string;
+  show: boolean;
+  onClick: (x: boolean) => void;
+}
+
+const AlertDelete = ({ data, show, onClick }: AlertDeleteProps) => {
+  const ref = useRef(null);
+
+  return (
+    <Modal _ref={ref} show={show} onClickOutside={() => onClick(false)}>
+      <div className="flex h-full w-full items-center justify-center">
+        <div
+          ref={ref}
+          className="flex flex-col items-center gap-5 rounded-xl bg-white p-5">
+          <span className="font-bold">{data}</span>
+          <span>Are you sure you want to delete?</span>
+          <div className="flex gap-5">
+            <button
+              onClick={() => onClick(false)}
+              className="rounded-xl bg-blue-500 px-5 py-3 text-xs font-bold text-white">
+              Cancel
+            </button>
+            <button
+              onClick={() => onClick(true)}
+              className="rounded-xl bg-slate-100 px-5 py-3 text-xs font-bold">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default AlertDelete;
